727d2f9867befd54554264433f631a63d5cd4e71
JabikU <enin.sacha@gmail.com>
2024-02-10 15:21:50 +0300

    цифорки

--- numbers.js
+++ numbers.js
@@ -1,1 +1,17 @@
-
+ function calculateSegmentProduct(A, B, C) {
+   return Math.abs((C - A) * (B - C));
+}
+ console.log(calculateSegmentProduct(0, 5, 2));
+//  export { calculateSegmentProduct };
+ function calculateKilobytes(a, b) {
+   return Math.floor(a / b);
+}
+ console.log(calculateKilobytes(5000, 1024));
+// const calculateDigitSum = (x1, x2) => Math.abs(x2-x1)
+//const calculateDigitSum = (x) => {
+//  const des = Math.floor(x/10);
+//const ed = x%10;
+//return des + ed;
+//}
+//console.log(des, ed)
+//export {calculateDigitSum}
